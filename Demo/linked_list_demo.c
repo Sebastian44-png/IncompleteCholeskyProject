5076bfb409db4cc146c210f34ebbe986ad7323a9
Sebastian <sebastian@pop-os.localdomain>
2021-09-10 15:00:21 +0200

    Added linked list datastructure, implemented icne0 using linked lists

---
+++ Demo/linked_list_demo.c
@@ -0,0 +1,45 @@
+#include "hpc.h"
+#include "hpc_linked_list.h"
+
+int main(){
+
+    node* head = create_slist();
+
+    node* current = head;
+
+    for (index i=0; i<10; i++){
+        current = add_node(current, i, (double)i+1);
+    }
+    print_list_data(head);
+
+    free(head);
+
+    node* head_list1 = create_slist();
+
+    node* current = head_list1;
+
+    for (index i=0; i<10; i++){
+        current = add_node(current, i, (double)i+1);
+    }
+    
+    node* head_list2 = create_slist();
+
+    current = head_list2;
+
+    for (index i=0; i<10; i++){
+        current = add_node(current, i*i, (double)i+1);
+    }
+
+    node** cols = malloc(sizeof(node*)*2);
+    cols[0] = head_list1;
+    cols[1] = head_list2;
+
+    for(int i =0; i< 2; i++){
+        print_list_data(cols[i]);
+    }
+
+    free(head_list1);
+    free(head_list2);
+}
+
+}
---
+++ Demo/sed_icne0_demo.c
@@ -0,0 +1,59 @@
+#include "hpc.h"
+
+#include<time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+struct timeval tv[50];
+#define TIME_SAVE(j)   (gettimeofday(&tv[j], (struct timezone*)0))
+#define TIME_ELAPSED(j,k)	(1.E+6*(tv[k].tv_sec-tv[j].tv_sec)+(tv[k].tv_usec-tv[j].tv_usec))
+
+int main (int argc, char **argv)
+{
+    index j;
+    index n;
+    index N;
+
+    double *b;
+    double *b_ref;
+    double *b_sol;
+    double *x; 
+
+    cs *A_cs;
+    sed *A_sed;
+    gem *A_gem;
+
+    printf("\n========================================\n");
+    /* Get number N as parameter */ 
+    N = 0;
+    if ( argc > 1 ) {
+        if ( (atoi(argv[1]) > 0) & (atoi(argv[1]) < 1000) ) { 
+            N = atoi(argv[1]);
+        }
+    } 
+   
+    A_cs = cs_lapmat_p1_square(N);  
+    A_sed = sed_compress(A_cs);
+    A_gem = gem_compress(A_cs);
+
+    printf("gem_Matrix \n");
+    gem_print(A_gem, 0);
+    
+    //printf("sed_Matrix \n");
+    //sed_print(A_sed, 0); 
+    printf("Buffers :");
+    printf("x: "); print_buffer_double(A_sed->x, A_sed->nzmax);
+    printf("ind: "); print_buffer_int(A_sed->i, A_sed->nzmax);
+    
+    sed* L;
+
+    sed_icne0(A_sed, 2.0, L);
+    sed_print(L, 0);
+    
+    cs_free (A_cs); 
+    sed_free(A_sed);
+    gem_free(A_gem);
+    free(b);
+
+    return (0) ;
+}
